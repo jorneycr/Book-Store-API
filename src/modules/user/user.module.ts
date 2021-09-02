bc96d17b4adb33721bcbba91da5605573e480fd2
jorneylm <jorney95@live.com>
2021-09-02 10:28:19 -0600

    shared modules and dto

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { UserService } from './user.service';
 
 @Module({
-    imports:[TypeOrmModule.forFeature([UserRepository])]
+    imports:[TypeOrmModule.forFeature([UserRepository])],
+    providers: [UserService]
 })
 export class UserModule {}
