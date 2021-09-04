30a2bebe6f661f3d14b5cd5ebcd79a9606e05735
jorneylm <jorney95@live.com>
2021-09-04 02:02:56 -0600

    Add jwt

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from 'src/config/config.service';
 import { AuthController } from './auth.controller';
 import { AuthRepository } from './auth.repository';
 import { AuthService } from './auth.service';
@@ -7,6 +8,6 @@ import { AuthService } from './auth.service';
 @Module({
   imports:[TypeOrmModule.forFeature([AuthRepository])],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, ConfigService]
 })
 export class AuthModule {}
